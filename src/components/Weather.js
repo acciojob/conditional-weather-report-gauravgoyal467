8f46543565eb364c4584a91a6f35b38354314c33
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-17 10:11:05 +0000

    5th try

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,18 +4,18 @@ import React from "react";
 const Weather = ({data}) => {
   let  style={};
   if(data.temperature>20){
-      style={
-          backgroundColor:"red"
-      }
-  }else{
-      style={
-          backgroundColor:"blue"
-      }
-  }
+    style={
+        backgroundColor: 'rgb(255, 0, 0)'
+    }
+}else{
+    style={
+        backgroundColor:'rgb(0, 0, 255)'
+    }
+}
 
   return (
     <div className="container">
-        <p>Temperature: {data.temperature}</p>
+        <span>Temperature: {data.temperature}</span>
         <span>{data.conditions}</span>
     </div>
   )
